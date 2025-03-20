f202ae59fdd7cf9d360562d063c217178c97bbd1
H-M-Nizum <hmnizum1714032@gmail.com>
2025-03-20 13:10:42 +0600

    Configure cors policy

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const connectDB = require("./config");
 require("dotenv").config();
-// import Router
 const rootRouter = require("./routes/rootRoute");
 const userRouter = require("./routes/userRouter");
 const logger = require("./middlewares/logger");
 const globalErrorHandeler = require("./middlewares/global-error-handeler");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 5000;
 
@@ -17,6 +17,21 @@ connectDB();
 
 // Middleware to parse JSON requests
 app.use(express.json());
+
+// Cors
+const allowedOrigins = [
+  "http://127.0.0.1:5500", // Local development
+  "http://localhost:3000", // Example: React frontend
+  "https://your-production-site.com", // Production domain
+];
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, // Allow cookies and authentication headers
+  })
+);
+
 // Global custom Middleware
 app.use(logger);
 app.use(globalErrorHandeler);
